714fd66d0f669c6f4dd9b50719823c4bec8b4140
aalokdevk29 <aalokdevk29@gmail.com>
2021-07-09 14:52:38 +0530

    setup router and  create static component page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import './App.css';
 
-function App() {
+const CryptoTrack = React.lazy(() => import('./components/cryptoTrack'));
+
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <React.Suspense fallback={'Loading...'}>
+        <Switch>
+          <Route
+            exact
+            path='/'
+            name='CryptoTrack App'
+            render={(props) => <CryptoTrack {...props} />}
+          />
+        </Switch>
+      </React.Suspense>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/cryptoTrack/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CryptoTrack = (props) => {
+  return <div></div>;
+};
+
+export default CryptoTrack;
